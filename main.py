b34e8e79fcf5f443138f728f9129921ce2b8006a
Jhon Cedeno <jhonalexandrocm@gmail.com>
2024-09-13 22:21:23 -0500

    Mejora del script de escritura automatizada con combinación aleatoria de palabras y ajuste de tiempos

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import pyautogui
 import time
+import random
 
-lista = ["uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve", "diez", 
-         "once", "doce", "trece", "catorce", "quince", "dieciséis", "diecisiete", "dieciocho", 
+lista = ["uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve", "diez", "once","doce", "trece", "catorce", "quince", "dieciséis", "diecisiete", "dieciocho", 
          "diecinueve", "veinte", "veintiuno", "veintidós", "veintitrés", "veinticuatro", 
          "veinticinco", "veintiséis", "veintisiete", "veintiocho", "veintinueve", "treinta"]
 
 time.sleep(3)
 
 for palabra in lista:
+    palabra = palabra + " " + random.choice(lista)
     for letra in palabra:
         pyautogui.press(letra)
         time.sleep(0.08)
     pyautogui.press('enter')
-    time.sleep(2.5)
+    time.sleep(4)
